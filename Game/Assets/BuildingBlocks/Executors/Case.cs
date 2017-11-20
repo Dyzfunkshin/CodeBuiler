f5d6b1cde7823421c2c44faa7017162aaaaab55e
Dyzfunkshin <bigmac3702@aol.com>
2017-11-19 20:16:35 -0600

    Implemented Switch.cs and added Case.cs

---
+++ Game/Assets/BuildingBlocks/Executors/Case.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Case : MonoBehaviour, IExecutor
+{
+    #region Constants
+
+    #endregion
+
+    #region Fields
+
+    public IType caseValue;
+
+    public List<IExecutable> executables;
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+
+    public bool Compile()
+    {
+        if (executables == null || caseValue == null)
+            return false;
+
+        foreach (IExecutable exec in executables)
+            if (!exec.Compile())
+                return false;
+
+        return true;
+    }
+
+    public void Execute()
+    {
+        if (executables == null)
+            return;
+
+        foreach (IExecutable executable in executables)
+            executable.Execute();
+    }
+
+    #endregion
+}
--- Game/Assets/BuildingBlocks/Executors/Switch.cs
+++ Game/Assets/BuildingBlocks/Executors/Switch.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Switch : MonoBehaviour, IExecutor
 {
@@ -8,6 +8,10 @@ public class Switch : MonoBehaviour, IExecutor
     #endregion
 
     #region Fields
+
+    public IType switchValueType;
+
+    public List<Case> cases;
 
     #endregion
 
@@ -21,9 +25,130 @@ public class Switch : MonoBehaviour, IExecutor
 
     #region Methods
 
-    public void Execute() { }
+    public void Execute()
+    {
+        if (switchValueType == null || cases == null)
+            return;
 
-    public bool Compile() { return true; }
+        Boolean switchTypeBoolean = TryTypeBoolean();
+        Decimal switchTypeDecimal = TryTypeDecimal();
+        Integer switchTypeInteger = TryTypeInteger();
+        String switchTypeString = TryTypeString();
+
+        foreach (Case _case in cases)
+        {
+            if (switchTypeBoolean != null)
+            {
+                Boolean val = _case.caseValue as Boolean;
+
+                if (val != null && val.Value == switchTypeBoolean.Value)
+                {
+                    _case.Execute();
+                    return;
+                }
+            }
+            else if (switchTypeDecimal != null && switchTypeDecimal.Value != null)
+            {
+                Decimal val = _case.caseValue as Decimal;
+
+                if (val != null && 
+                    val.Value != null && 
+                    Mathf.Approximately(val.Value.Value, switchTypeDecimal.Value.Value))
+                {
+                    _case.Execute();
+                    return;
+                }
+            }
+            else if (switchTypeInteger != null)
+            {
+                Integer val = _case.caseValue as Integer;
+
+                if (val != null && val.Value == switchTypeInteger.Value)
+                {
+                    _case.Execute();
+                    return;
+                }
+            }
+            else if (switchTypeString != null)
+            {
+                String val = _case.caseValue as String;
+
+                if (val != null && val.Value == switchTypeString.Value)
+                {
+                    _case.Execute();
+                    return;
+                }
+            }
+
+            _case.Execute();
+        }
+    }
+
+    public bool Compile()
+    {
+        if (switchValueType == null || cases == null)
+            return false;
+
+        return CompileCases();
+    }
+
+    private bool CompileCases()
+    {
+        Boolean switchTypeBoolean = TryTypeBoolean();
+        Decimal switchTypeDecimal = TryTypeDecimal();
+        Integer switchTypeInteger = TryTypeInteger();
+        String switchTypeString = TryTypeString();
+
+        foreach (Case _case in cases)
+        {
+            if (switchTypeBoolean != null)
+            {
+                Boolean val = _case.caseValue as Boolean;
+                if (val == null)
+                    return false;
+            }
+            else if (switchTypeDecimal != null)
+            {
+                Decimal val = _case.caseValue as Decimal;
+                if (val == null)
+                    return false;
+            }
+            else if (switchTypeInteger != null)
+            {
+                Integer val = _case.caseValue as Integer;
+                if (val == null)
+                    return false;
+            }
+            else if (switchTypeString != null)
+            {
+                String val = _case.caseValue as String;
+                if (val == null)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private Boolean TryTypeBoolean()
+    {
+        return switchValueType as Boolean;
+    }
+
+    private Decimal TryTypeDecimal()
+    {
+        return switchValueType as Decimal;
+    }
+
+    private Integer TryTypeInteger()
+    {
+        return switchValueType as Integer;
+    }
+
+    private String TryTypeString()
+    {
+        return switchValueType as String;
+    }
 
     #endregion
 }
